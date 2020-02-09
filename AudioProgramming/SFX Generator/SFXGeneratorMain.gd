655717aafb274427947a52fc788186378f0224bd
Brandon Schmeidler <brandon.bsgstudio@gmail.com>
2020-02-09 15:19:19 -0600

    I think I've worked out how projects will be created. No saving/loading yet.

--- AudioProgramming/SFX Generator/SFXGeneratorMain.gd
+++ AudioProgramming/SFX Generator/SFXGeneratorMain.gd
@@ -1,5 +1,6 @@
 extends Control
 
+var root:VBoxContainer = null
 var project_tabs:Tabs = null
 var current_project_editor:GraphEdit = null
 var add_node_popup_menu:PopupMenu = null
@@ -14,7 +15,7 @@ func add_node(id:int):
 	node.offset = get_global_mouse_position() - current_project_editor.rect_global_position + current_project_editor.scroll_offset
 	current_project_editor.add_child(node)
 
-func add_new_project():
+func add_project():
 	# if project exists already
 	if (current_project_editor != null):
 	#	hide current_project_editor
@@ -26,28 +27,60 @@ func add_new_project():
 	current_project_editor.size_flags_horizontal = SIZE_EXPAND_FILL
 	current_project_editor.size_flags_vertical = SIZE_EXPAND_FILL
 	current_project_editor.connect("popup_request",self,"_on_GraphEdit_popup_request")
-	$RootVBoxContainer.add_child(current_project_editor, true)
+	root.add_child(current_project_editor, true)
 	
 	# create new tab in project_tabs with project name
 	project_tabs.add_tab(current_project_editor.name)
 	project_tabs.current_tab = project_tabs.get_tab_count()-1
 	
+	print(root.get_children())
+
+func close_project(name:String):
+	var existing_projects = root.get_children()
+	var current_project = null
+	for project in existing_projects:
+		if (project.name == name):
+			project.queue_free()
+			break
+
+func change_project_tab(tab:int):
+	current_project_editor.visible = false
+	var name = project_tabs.get_tab_title(tab)
+	var existing_projects = root.get_children()
+	for child in existing_projects:
+		if (child.name == name):
+			current_project_editor = child
+			current_project_editor.visible = true
+			print(current_project_editor.name)
+			break
 
 func _ready():
 	project_tabs = $RootVBoxContainer/Tabs
 	add_node_popup_menu = $AddNodePopupMenu
+	root = $RootVBoxContainer
 	
-	add_new_project()
-
+	add_project()
 
 func _on_GraphEdit_popup_request(position:Vector2):
 	add_node_popup_menu.rect_position = position
 	add_node_popup_menu.show_modal()
 	print(position)
 
-
 func _on_AddNodePopupMenu_id_pressed(id:int):
 	add_node(id)
 
 func _on_FileMenuButton_new_project_requested():
-	add_new_project()
+	add_project()
+
+func _on_Tabs_tab_changed(tab:int):
+	change_project_tab(tab)
+
+func _on_Tabs_tab_close(tab:int):
+	var name = project_tabs.get_tab_title(tab)
+	close_project(name)
+	project_tabs.remove_tab(tab)
+	
+	if (project_tabs.current_tab > -1):
+		change_project_tab(project_tabs.current_tab)
+	else:
+		current_project_editor = null
